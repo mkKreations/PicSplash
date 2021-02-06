945b44b44c676b0079c33ebafeaa739241e87118
mkKreations <63332239+mkKreations@users.noreply.github.com>
2021-02-06 18:46:37 -0500

    Added HomeImageCell file

---
+++ PicSplash/PicSplash/Cells/HomeImageCell.swift
@@ -0,0 +1,17 @@
+//
+//  HomeImageCell.swift
+//  PicSplash
+//
+//  Created by Marcus on 2/6/21.
+//
+
+import UIKit
+
+class HomeImageCell: UICollectionViewCell {
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+	required init?(coder: NSCoder) {
+		fatalError("Crash in HomeImageCell")
+	}
+}
