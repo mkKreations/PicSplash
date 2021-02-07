2d5682fbb19bbe04f1b379c499d590d892d7e5e9
mkKreations <63332239+mkKreations@users.noreply.github.com>
2021-02-06 19:50:25 -0500

    Added compositionalLayout & diffableDatasource logic to collection view

--- PicSplash/PicSplash/Controllers/HomeViewController.swift
+++ PicSplash/PicSplash/Controllers/HomeViewController.swift
@@ -8,17 +8,31 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+	// instance vars
 	private lazy var collectionView: UICollectionView = {
-		UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+		UICollectionView(frame: .zero, collectionViewLayout: configureCompositionalLayout())
 	}()
+	private var datasource: UICollectionViewDiffableDataSource<Section, ImagePlaceholder>!
+		
+	
+	// collection view section enum
+	enum Section {
+		case main
+	}
+	
+	
+	// MARK: view life cycle
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		view.backgroundColor = .systemYellow
-		
 		configureCollectionView()
+		configureDatasource()
+		applySnapshot()
 	}
+	
+	
+	// MARK: collection view config
 	
 	private func configureCollectionView() {
 		collectionView.translatesAutoresizingMaskIntoConstraints = false
@@ -29,6 +43,41 @@ class HomeViewController: UIViewController {
 		collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
 		collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
 		collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+	}
+	
+	private func configureCompositionalLayout() -> UICollectionViewCompositionalLayout {
+		let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+																					heightDimension: .estimated(100.0))
+		let item = NSCollectionLayoutItem(layoutSize: itemSize)
+		
+		let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+																					 heightDimension: .estimated(100.0))
+		let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize,
+																								 subitems: [item])
+		
+		let section = NSCollectionLayoutSection(group: group)
+		
+		return UICollectionViewCompositionalLayout(section: section)
+	}
+	
+	private func configureDatasource() {
+		datasource = UICollectionViewDiffableDataSource(collectionView: collectionView, cellProvider: {
+			(collectionView, indexPath, imagePlaceholder) -> UICollectionViewCell? in
+			guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeImageCell.reuseIdentifier,
+																													for: indexPath) as? HomeImageCell else { return nil }
+			
+			cell.displayText = String(samplePics[indexPath.row].height)
+			cell.displayBackgroundColor = samplePics[indexPath.row].placeholderColor
+			
+			return cell
+		})
+	}
+	
+	private func applySnapshot() {
+		var snapshot = NSDiffableDataSourceSnapshot<Section, ImagePlaceholder>()
+		snapshot.appendSections([.main])
+		snapshot.appendItems(samplePics, toSection: .main)
+		datasource.apply(snapshot)
 	}
 	
 	
