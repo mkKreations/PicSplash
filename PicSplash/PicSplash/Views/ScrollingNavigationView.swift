2aa9c8b6cd5954bdeb965ddf6bf37d42cf254812
mkKreations <63332239+mkKreations@users.noreply.github.com>
2021-02-07 14:46:26 -0500

    Added ScrollingNavigationView file

---
+++ PicSplash/PicSplash/Views/ScrollingNavigationView.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollingNavigationView.swift
+//  PicSplash
+//
+//  Created by Marcus on 2/7/21.
+//
+
+import UIKit
+
+class ScrollingNavigationView: UIView {
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+	required init?(coder: NSCoder) {
+		fatalError("Crash in ScrollingNavigationView")
+	}
+}
