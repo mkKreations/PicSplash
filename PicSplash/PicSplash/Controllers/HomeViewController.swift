8acc702e13714f54451939390bfb659491206648
mkKreations <63332239+mkKreations@users.noreply.github.com>
2021-02-06 19:12:48 -0500

    Added basic collectionView to HomeVC - no layout/datasource

--- PicSplash/PicSplash/Controllers/HomeViewController.swift
+++ PicSplash/PicSplash/Controllers/HomeViewController.swift
@@ -8,11 +8,28 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+	private lazy var collectionView: UICollectionView = {
+		UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+	}()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
 		view.backgroundColor = .systemYellow
+		
+		configureCollectionView()
 	}
+	
+	private func configureCollectionView() {
+		collectionView.translatesAutoresizingMaskIntoConstraints = false
+		collectionView.register(HomeImageCell.self, forCellWithReuseIdentifier: HomeImageCell.reuseIdentifier)
+		view.addSubview(collectionView)
+		
+		collectionView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+		collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+		collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+		collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+	}
+	
 	
 }
