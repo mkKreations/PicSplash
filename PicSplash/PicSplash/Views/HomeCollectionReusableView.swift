fe0758faa6445ca357dec1ee3ffa087abe54e041
mkKreations <63332239+mkKreations@users.noreply.github.com>
2021-02-06 22:47:06 -0500

    Added HomeCollectionReusableView implementation

--- PicSplash/PicSplash/Views/HomeCollectionReusableView.swift
+++ PicSplash/PicSplash/Views/HomeCollectionReusableView.swift
@@ -12,9 +12,51 @@ class HomeCollectionReusableView: UICollectionReusableView {
 	static let reuseIdentifier: String = UUID().uuidString
 	
 	
+	// instance vars
+	private let displayLabel: UILabel = UILabel(frame: .zero)
+	private var labelLeadingConstraint: NSLayoutConstraint!
+	
+	
+	// enum to represent our different styles
+	enum DisplayStyle {
+		case large
+		case small
+	}
+	
+	
+	// public setters
+	var displayStyle: DisplayStyle = .large {
+		didSet {
+			switch displayStyle {
+			case .large:
+				labelLeadingConstraint.constant = 15.0
+			case .small:
+				labelLeadingConstraint.constant = 10.0
+			}
+		}
+	}
+	
+	var displayText: String? {
+		didSet {
+			if let text = displayText {
+				displayLabel.text = text
+			}
+		}
+	}
+	
+	
 	// inits
 	override init(frame: CGRect) {
 		super.init(frame: frame)
+		
+		displayLabel.translatesAutoresizingMaskIntoConstraints = false
+		displayLabel.textColor = .white
+		addSubview(displayLabel)
+		
+		displayLabel.topAnchor.constraint(equalTo: topAnchor).isActive = true
+		displayLabel.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+		labelLeadingConstraint = displayLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15.0)
+		labelLeadingConstraint.isActive = true
 	}
 	required init?(coder: NSCoder) {
 		fatalError("Crash in HomeCollectionReusableView")
