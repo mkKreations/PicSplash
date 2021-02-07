a0bd546a3325de54c5df026f83533cf64e25a828
mkKreations <63332239+mkKreations@users.noreply.github.com>
2021-02-06 19:02:47 -0500

    Added ImageShadowOverlayView file - restructured Views/Cells repo

---
+++ PicSplash/PicSplash/Views/ImageShadowOverlayView.swift
@@ -0,0 +1,17 @@
+//
+//  ImageShadowOverlayView.swift
+//  PicSplash
+//
+//  Created by Marcus on 2/6/21.
+//
+
+import UIKit
+
+class ImageShadowOverlayView: UIView {
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+	required init?(coder: NSCoder) {
+		fatalError("Crash in ImageShadowOverlayView")
+	}
+}
