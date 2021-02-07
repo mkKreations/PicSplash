179d3f31c4c65339c8503d54aa01ca2a9b568971
mkKreations <63332239+mkKreations@users.noreply.github.com>
2021-02-06 21:44:14 -0500

    Added HomeOrthogonalCell file

---
+++ PicSplash/PicSplash/Views/Cells/HomeOrthogonalCell.swift
@@ -0,0 +1,18 @@
+//
+//  HomeOrthogonalCell.swift
+//  PicSplash
+//
+//  Created by Marcus on 2/6/21.
+//
+
+import UIKit
+
+class HomeOrthogonalCell: UICollectionViewCell {
+	// inits
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+	required init?(coder: NSCoder) {
+		fatalError("Crash in HomeOrthogonalCell")
+	}
+}
