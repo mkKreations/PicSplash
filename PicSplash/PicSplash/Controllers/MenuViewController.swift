a9d8fc4f19376d231bc78e3efbe5f62824b2f43e
mkKreations <63332239+mkKreations@users.noreply.github.com>
2021-02-09 15:00:42 -0500

    Added MenuTopView to MenuViewController

--- PicSplash/PicSplash/Controllers/MenuViewController.swift
+++ PicSplash/PicSplash/Controllers/MenuViewController.swift
@@ -8,9 +8,27 @@
 import UIKit
 
 class MenuViewController: UIViewController {
+	private let topView: MenuTopView = MenuTopView(frame: .zero)
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		view.backgroundColor = .systemYellow
+		view.backgroundColor = .picsplashBlack
+
+		configureSubviews()
+		constrainSubviews()
+	}
+	
+	
+	// MARK: view helpers
+	
+	private func configureSubviews() {
+		topView.translatesAutoresizingMaskIntoConstraints = false
+		view.addSubview(topView)
+	}
+	
+	private func constrainSubviews() {
+		topView.topAnchor.constraint(equalTo: view.topAnchor, constant: 40.0).isActive = true
+		topView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
 	}
 }
