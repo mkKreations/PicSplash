dd0751e0896ed833c9a9f0ba3fe8192bb8ffe9fc
mkKreations <63332239+mkKreations@users.noreply.github.com>
2021-02-09 13:42:23 -0500

    Added MenuViewController file

---
+++ PicSplash/PicSplash/Controllers/MenuViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MenuViewController.swift
+//  PicSplash
+//
+//  Created by Marcus on 2/9/21.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
