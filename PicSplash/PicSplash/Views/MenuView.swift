26ea92532ba81d609f80a315cc77dbbe02e1cde4
mkKreations <63332239+mkKreations@users.noreply.github.com>
2021-02-09 15:04:48 -0500

    Added MenuView file

---
+++ PicSplash/PicSplash/Views/MenuView.swift
@@ -0,0 +1,19 @@
+//
+//  MenuView.swift
+//  PicSplash
+//
+//  Created by Marcus on 2/9/21.
+//
+
+import UIKit
+
+class MenuView: UIView {
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		
+		backgroundColor = .systemRed
+	}
+	required init?(coder: NSCoder) {
+		fatalError("Crash in MenuView")
+	}
+}
