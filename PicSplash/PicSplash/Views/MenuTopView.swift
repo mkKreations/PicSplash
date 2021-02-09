13ef11f7f4b349b3706a5ed66ff0ac6523cc7bf1
mkKreations <63332239+mkKreations@users.noreply.github.com>
2021-02-09 14:26:10 -0500

    Added MenuTopView file

---
+++ PicSplash/PicSplash/Views/MenuTopView.swift
@@ -0,0 +1,17 @@
+//
+//  MenuTopView.swift
+//  PicSplash
+//
+//  Created by Marcus on 2/9/21.
+//
+
+import UIKit
+
+class MenuTopView: UIView {
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+	required init?(coder: NSCoder) {
+		fatalError("Crash in MenuTopView")
+	}
+}
