65fd73671126a9ff8686fbfdfb0e6eb588361d5b
mkKreations <63332239+mkKreations@users.noreply.github.com>
2021-02-09 14:54:30 -0500

    Added UIColor+extensions file with picsplashBlack UIColor

---
+++ PicSplash/PicSplash/Extensions/UIColor+extensions.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+extensions.swift
+//  PicSplash
+//
+//  Created by Marcus on 2/9/21.
+//
+
+import UIKit
+
+extension UIColor {
+	static var picsplashBlack: UIColor {
+		UIColor(white: 0.09, alpha: 1.0)
+	}
+}
