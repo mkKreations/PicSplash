736daf45557db6bf69a02d535087c64e6a0f3b26
mkKreations <63332239+mkKreations@users.noreply.github.com>
2021-02-09 21:12:54 -0500

    Added LoginView file

---
+++ PicSplash/PicSplash/Views/LoginView.swift
@@ -0,0 +1,17 @@
+//
+//  LoginView.swift
+//  PicSplash
+//
+//  Created by Marcus on 2/9/21.
+//
+
+import UIKit
+
+class LoginView: UIView {
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+	required init?(coder: NSCoder) {
+		fatalError("Crash in LoginView")
+	}
+}
