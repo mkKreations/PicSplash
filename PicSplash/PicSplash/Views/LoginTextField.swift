ecacdfb9c91adcf1e1b0b29e0c097567107d1dba
mkKreations <63332239+mkKreations@users.noreply.github.com>
2021-02-10 11:58:09 -0500

    Added LoginTextField file

---
+++ PicSplash/PicSplash/Views/LoginTextField.swift
@@ -0,0 +1,17 @@
+//
+//  LoginTextField.swift
+//  PicSplash
+//
+//  Created by Marcus on 2/10/21.
+//
+
+import UIKit
+
+class LoginTextField: UIView {
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+	required init?(coder: NSCoder) {
+		fatalError("Crash in LoginTextField")
+	}
+}
