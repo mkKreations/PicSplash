5915d51fbded2232b907409d7751a849fe2a9c1a
mkKreations <63332239+mkKreations@users.noreply.github.com>
2021-02-11 17:45:32 -0500

    Added DetailViewController file

---
+++ PicSplash/PicSplash/Controllers/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.swift
+//  PicSplash
+//
+//  Created by Marcus on 2/11/21.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		view.backgroundColor = .systemYellow
+	}
+}
