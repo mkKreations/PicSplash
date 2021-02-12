ae6faf919b286719c2d55f7bdf4552f6591da4c3
mkKreations <63332239+mkKreations@users.noreply.github.com>
2021-02-12 10:27:25 -0500

    Added DetailActionButton file

---
+++ PicSplash/PicSplash/Views/DetailActionButton.swift
@@ -0,0 +1,17 @@
+//
+//  DetailActionButton.swift
+//  PicSplash
+//
+//  Created by Marcus on 2/12/21.
+//
+
+import UIKit
+
+class DetailActionButton: UIButton {
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+	required init?(coder: NSCoder) {
+		fatalError("Crash in DetailActionButton")
+	}
+}
