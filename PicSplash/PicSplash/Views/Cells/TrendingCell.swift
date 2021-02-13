004b7d543ad24d05baada64ea10f8b0a363977a9
mkKreations <63332239+mkKreations@users.noreply.github.com>
2021-02-13 18:38:17 -0500

    Added TrendingCell file

---
+++ PicSplash/PicSplash/Views/Cells/TrendingCell.swift
@@ -0,0 +1,17 @@
+//
+//  TrendingCell.swift
+//  PicSplash
+//
+//  Created by Marcus on 2/13/21.
+//
+
+import UIKit
+
+class TrendingCell: UICollectionViewCell {
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+	required init?(coder: NSCoder) {
+		fatalError("Crash in TrendingCell")
+	}
+}
