f859c8454d6cf5c35dfdb86b43cf158197506755
mkKreations <63332239+mkKreations@users.noreply.github.com>
2021-02-13 23:07:41 -0500

    Added TrendingReusableView file

---
+++ PicSplash/PicSplash/Views/TrendingReusableView.swift
@@ -0,0 +1,17 @@
+//
+//  TrendingReusableView.swift
+//  PicSplash
+//
+//  Created by Marcus on 2/13/21.
+//
+
+import UIKit
+
+class TrendingReusableView: UICollectionReusableView {
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+	}
+	required init?(coder: NSCoder) {
+		fatalError("Crash in TrendingReusableView")
+	}
+}
