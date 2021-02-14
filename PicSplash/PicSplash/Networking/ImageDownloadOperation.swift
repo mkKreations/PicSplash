4f38a74ffbd3ca26d57d4708c104bdcab701192f
mkKreations <63332239+mkKreations@users.noreply.github.com>
2021-02-14 10:25:52 -0500

    Added ImageDownloadOperation file

---
+++ PicSplash/PicSplash/Networking/ImageDownloadOperation.swift
@@ -0,0 +1,13 @@
+//
+//  ImageDownloadOperation.swift
+//  PicSplash
+//
+//  Created by Marcus on 2/14/21.
+//
+
+import Foundation
+
+// our Operation for asynchronously downloading images
+
+class ImageDownloadOperation: AsyncOperation {
+}
