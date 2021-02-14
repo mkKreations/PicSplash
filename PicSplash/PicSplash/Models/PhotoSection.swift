5f9102920d3e3596dad8f276768f6bf3db285218
mkKreations <63332239+mkKreations@users.noreply.github.com>
2021-02-14 15:44:01 -0500

    Added PhotoSection file

---
+++ PicSplash/PicSplash/Models/PhotoSection.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoSection.swift
+//  PicSplash
+//
+//  Created by Marcus on 2/14/21.
+//
+
+import Foundation
+
+struct PhotoSection {
+	let id: UUID = UUID() // unique ID to help distinguish value type instances
+	let title: String
+//	let items: 
+}
