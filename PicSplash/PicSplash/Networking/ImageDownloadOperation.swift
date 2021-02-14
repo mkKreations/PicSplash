10c5235c588064d6bc5ac4ca1002751ae77b5fc6
mkKreations <63332239+mkKreations@users.noreply.github.com>
2021-02-14 10:38:04 -0500

    Added ImageDownloadOperation base implementation

--- PicSplash/PicSplash/Networking/ImageDownloadOperation.swift
+++ PicSplash/PicSplash/Networking/ImageDownloadOperation.swift
@@ -5,9 +5,45 @@
 //  Created by Marcus on 2/14/21.
 //
 
-import Foundation
+import UIKit
 
 // our Operation for asynchronously downloading images
 
+// we're not handling any cancelling currently, we're
+// managing Operation instances within NetworkingManager
+
 class ImageDownloadOperation: AsyncOperation {
+	private let url: URL
+	
+	init(url: URL) {
+		self.url = url
+		super.init()
+	}
+	
+	override func main() {
+		URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+			guard let self = self else { return }
+			
+			defer { self.state = .finished } // make sure we update our state before handing off control
+			
+			if let _ = error {
+				// call completion here and pass thru error
+				return
+			}
+			
+			guard let httpResponse = response as? HTTPURLResponse,
+						(200...300).contains(httpResponse.statusCode) else {
+				// call completion here and pass thru error
+				return
+			}
+			
+			guard let data = data,
+						let _ = UIImage(data: data) else {
+				// call completion here and pass thru error
+				return
+			}
+			
+			// call completion handler and pass thru image
+		}
+	}
 }
