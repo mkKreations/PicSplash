229468ef20ec22af21447c657a87437dba79e294
mkKreations <63332239+mkKreations@users.noreply.github.com>
2021-02-15 22:12:15 -0500

    Updated NetworkingManager to use InitialCollectionViewDataLoadOperation

--- PicSplash/PicSplash/Networking/NetworkingManager.swift
+++ PicSplash/PicSplash/Networking/NetworkingManager.swift
@@ -66,6 +66,7 @@ class NetworkingManager {
 		requestUrlString.append(Self.homeImagesListPath)
 		requestUrlString.append(Self.clientIDPath)
 		
+		// ensure we have valid Url
 		guard let requestUrl = URL(string: requestUrlString) else {
 			completion(.invalidUrl)
 			return
@@ -73,32 +74,20 @@ class NetworkingManager {
 
 		print("OUR URL: \(requestUrl)")
 		
-		URLSession.shared.dataTask(with: requestUrl) { [weak self] data, response, error in
-			guard let self = self else { return }
-			
-			// process our home images list data
-			try? self.processHomeImagesListData(data: data, urlResponse: response, error: error)
-			
-			// draw and cache all blurred images
-			// for our Photo objects before handing
-			// control back over to the UI so we can
-			// show blurred images for Photo while we
-			// lazily load actual image
-			self.homeImagesSections.forEach { section in
-				section.items.forEach { photo in
-					// we're adding these operations to imageDownloadQueue
-					self.drawAndCacheBlurredImage(usingBlurHashString: photo.blurString)
-				}
-			}
-			
-			// adding a barrierBlock ensures that all previous tasks on this queue will be completed
-			// before running this task. within here, we're just calling our completion block to hand
-			// control back over to the UI - important to deploy barrier block on correct queue
-			self.imageDownloadQueue.addBarrierBlock {
-				completion(nil)
-			}
-						
-		}.resume()
+		// create and add each Operation required
+		// to load all home data for initial load
+		let initialCollectionViewDataLoadOperation = InitialCollectionViewDataLoadOperation(initialDataToLoadUrl: requestUrl)
+		initialCollectionViewDataLoadOperation.delegate = self
+		self.imageDownloadQueue.addOperation(initialCollectionViewDataLoadOperation)
+		
+		// once all data fetching Operations have been added to queue
+		// add this BarrierBlock to the queue which requires
+		// that all previously added tasks must be completed
+		// in order for this task to be executed - we're
+		// simply handing control back off to the UI
+		self.imageDownloadQueue.addBarrierBlock {
+			completion(nil)
+		}
 	}
 	
 	func blurredImage(forBlurHashString blurHash: String) -> UIImage? {
@@ -151,80 +140,25 @@ class NetworkingManager {
 		}
 	}
 	
-	
-	
-	// MARK: helpers
-	
-	private func drawAndCacheBlurredImage(usingBlurHashString blurHashString: String) {
-		let blurHashOperation = BlurHashOperation(blurHash: blurHashString)
-		blurHashOperation.queuePriority = .veryHigh
-		blurHashOperation.completionBlock = {
-			// make sure the operation completed & we have a blurredImage
-			guard let blurredImage = blurHashOperation.blurredImage else { return }
-			
-			print("CACHING BLURRED IMAGE")
-			
-			// cache the image using the blur hash as the key
-			self.imageDownloadCache.setObject(blurredImage, forKey: NSString(string: blurHashString))
-		}
-		// add to image download queue
-		imageDownloadQueue.addOperation(blurHashOperation)
+}
+
+
+
+// MARK: NetworkingOperationsProtocol conformance
+
+extension NetworkingManager: NetworkingOperationsProtocol {
+	var homeSections: [PhotoSection] {
+		homeImagesSections
 	}
 	
-	private func processHomeImagesListData(data: Data?,
-																				 urlResponse response: URLResponse?,
-																				 error: Error?) throws {
-		// return and capture error from server
-		if let error = error {
-			throw NetworkingError.serverError(error)
-		}
-		
-		// ensure we have successful httpResponse
-		guard let httpResponse = response as? HTTPURLResponse,
-					(200...300).contains(httpResponse.statusCode) else {
-			throw NetworkingError.invalidResponse
-		}
-		
-		print("REQUESTS REMAINING: \(httpResponse.allHeaderFields["x-ratelimit-remaining"]) :(")
-			
-		// unpack data & deserialize response data into JSON
-		guard let data = data,
-					let jsonList = try? JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]] else {
-			throw NetworkingError.failedDeserialization
-		}
-		
-		// get the section we want to modify - we know we'll have them - no need to bail out
-		guard var photoExploreSection = homeImagesSections.filter({ $0.type == .explore }).first else { return }
-		guard var photoNewSection = homeImagesSections.filter({ $0.type == .new }).first else { return }
-
-		// the mapping of JSON data to our object is not 1 to 1 so we unpack the old fashioned way
-		// and append the data to our homeImagesSections - also handle blurHash
-		jsonList.forEach { item in
-			guard let blurHashString: String = item["blur_hash"] as? String,
-						let width: Int = item["width"] as? Int,
-						let height: Int = item["height"] as? Int,
-						let userDict: [String: Any] = item["user"] as? [String: Any],
-						let userName: String = userDict["username"] as? String,
-						let urlsDict: [String: String] = item["urls"] as? [String: String],
-						let imageUrl: String = urlsDict["small"]
-			else { return } // should I throw NetworkError here?
-			
-			photoExploreSection.items.append(Photo(imageUrl: imageUrl,
-																						 author: userName,
-																						 blurString: blurHashString,
-																						 height: height,
-																						 width: width))
-			photoNewSection.items.append(Photo(imageUrl: imageUrl,
-																				 author: userName,
-																				 blurString: blurHashString,
-																				 height: height,
-																				 width: width))
-			
-		}
-//		print("JSON DATA: \(jsonList)")
-		
-		// reset data on our property
-		homeImagesSections[0] = photoExploreSection
-		homeImagesSections[1] = photoNewSection
+	func cacheBlurredImage(_ blurredImage: UIImage, forBlurHash blurHash: String) {
+		// cache the image using the blur hash as the key
+		self.imageDownloadCache.setObject(blurredImage, forKey: NSString(string: blurHash))
+	}
+	
+	func loadedPhotoSection(_ photoSection: [Photo], forPhotoSectionType photoSectionType: PhotoSectionType) {
+		var section = homeImagesSections[photoSectionType.rawValue]
+		section.items = photoSection
+		homeImagesSections[photoSectionType.rawValue] = section
 	}
 }
