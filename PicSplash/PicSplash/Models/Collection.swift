7f1698642acf7da4140abcf6b49ce1f754cd2492
mkKreations <63332239+mkKreations@users.noreply.github.com>
2021-02-16 17:50:23 -0500

    Added Collection model with implementation

---
+++ PicSplash/PicSplash/Models/Collection.swift
@@ -0,0 +1,38 @@
+//
+//  Collection.swift
+//  PicSplash
+//
+//  Created by Marcus on 2/16/21.
+//
+
+import Foundation
+
+final class Collection {
+	let id: Int
+	let title: String
+	let blurHash: String
+	let imageUrl: String
+	
+	init(id: Int, title: String, blurHash: String, imageUrl: String) {
+		self.id = id
+		self.title = title
+		self.blurHash = blurHash
+		self.imageUrl = imageUrl
+	}
+}
+
+extension Collection: Hashable {
+	static func == (lhs: Collection, rhs: Collection) -> Bool {
+			lhs.id == rhs.id &&
+			lhs.title == rhs.title &&
+			lhs.blurHash == rhs.blurHash &&
+			lhs.imageUrl == rhs.imageUrl
+	}
+	
+	func hash(into hasher: inout Hasher) {
+		hasher.combine(id)
+		hasher.combine(title)
+		hasher.combine(blurHash)
+		hasher.combine(imageUrl)
+	}
+}
